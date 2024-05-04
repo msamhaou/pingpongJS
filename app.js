bc4148359a717165fcfdfb5c35b4b006a12444e9
Mohamed-Taha Samhaoui <msamhaou@e2r2p5.1337.ma>
2024-05-04 12:38:24 +0100

    player_collision_impemented

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ window.addEventListener('load', function(){
       this.rad = 15;
       this.collisionY = game.player.posY + game.player.height / 2;
       this.collisionX = game.player.posX + game.player.width + this.rad + 1;
+      this.angle = Math.random() % 2;
+      this.speed = 10;
+      this.dirX = this.speed * Math.cos(this.angle);
+      this.dirY = this.speed * Math.sin(this.angle);
     }
     draw(){
       ctx.beginPath();
@@ -25,24 +29,25 @@ window.addEventListener('load', function(){
       ctx.closePath();
     }
     iscollide(){
-      if (this.collisionX - this.rad > this.game.player.posX && this.collisionX - this.rad <= this.game.player.posX + this.game.player.width &&
+      if (this.collisionX - this.rad >= this.game.player.posX && this.collisionX - this.rad - this.speed <= this.game.player.posX + this.game.player.width &&
           this.collisionY >= this.game.player.posY && this.collisionY <= this.game.player.posY + this.game.player.height){
-        console.log("collide");
+            this.angle = ((this.collisionY - this.game.player.posY - this.game.player.height/2) * 45) / (this.game.player.height / 2);
+            this.angle = this.angle * Math.PI/180;
+            this.dirX = this.speed * Math.cos(this.angle);
+            this.dirY = this.speed * Math.sin(this.angle);
+            console.log(this.angle ,this.collisionY)
       }
     }
-    // update(){
-    //   if (this.collisionY + this.rad >= canvas.height || this.col == 1)
-    //     {
-    //       this.col = 1;
-    //       this.collisionY = this.collisionY + Math.sin( 7 *Math.PI / 4);
-    //       this.collisionX += Math.cos(Math.PI / 4);
-    //     }
-    //   else if (this.col == 0){
-    //     this.collisionX += Math.cos( (Math.PI / 4));
-    //     this.collisionY += Math.sin((Math.PI / 4));
-    //   }
-    //   console.log(this.collisionY)
-    // }
+    update(){
+      if ((this.collisionY + this.rad >= canvas.height || this.collisionY - this.rad <= 0)){
+          this.dirY *= -1;
+        }
+      else if ((this.collisionX + this.rad >= canvas.width || this.collisionX - this.rad <= 0)){
+        this.dirX *= -1;
+      }
+      this.collisionX += this.dirX;
+      this.collisionY += this.dirY;
+    }
   }
     
   class Player
@@ -55,7 +60,7 @@ window.addEventListener('load', function(){
         this.rad = 10;
         this.posX=20;
         this.posY = canvas.height/2 - this.height / 2;
-        // this.speed = 
+        this.speed = 5;
       }
       draw()
       {
@@ -83,10 +88,56 @@ window.addEventListener('load', function(){
       {
         if (this.posY > 0 && this.game.keyUp)
         {
-          this.posY -= this.game.speed;
+          this.posY -= this.speed;
         }
         if (this.posY < this.game.height - this.height && this.game.keyDown){
-          this.posY += this.game.speed;
+          this.posY += this.speed;
+        }
+      }
+  }
+
+  class Enemy
+  {
+      constructor(game)
+      {
+        this.game = game;
+        this.width =  30; 
+        this.height = 100;
+        this.rad = 10;
+        this.posX= canvas.width - 20 - this.width;
+        this.posY = canvas.height/2 - this.height / 2;
+        this.speed = 5;
+      }
+      draw()
+      {
+        ctx.beginPath();
+        ctx.strokeStyle = "black";
+        ctx.moveTo(this.posX,this.posY + this.rad);
+        ctx.arcTo(this.posX, this.posY + this.height,
+                 this.posX + this.width, this.posY + this.height, this.rad);
+        
+        ctx.arcTo(this.posX + this.width, this.posY + this.height,
+                 this.posX + this.width, this.posY, this.rad);
+        
+        ctx.arcTo(this.posX + this.width, this.posY,
+                 this.posX , this.posY, this.rad);
+        
+         ctx.arcTo(this.posX, this.posY,
+                 this.posX , this.posY + this.height, this.rad);
+        ctx.fillStyle = "red"
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+      }
+    
+      update()
+      {
+        if (this.posY > 0 && this.game.keyUp)
+        {
+          this.posY -= this.speed;
+        }
+        if (this.posY < this.game.height - this.height && this.game.keyDown){
+          this.posY += this.speed;
         }
       }
   }
@@ -97,10 +148,11 @@ window.addEventListener('load', function(){
       this.height = canvas.height;
       this.canvas= canvas;
       this.player = new Player(this);
+      this.enemy = new Enemy(this);
       this.ball = new Ball(this);
       this.keyUp = 0;
       this.keyDown = 0;
-      this.speed = 2;
+      this.speed = 5;
       this.mouseX = 0;
       this.mouseY = 0;
       window.addEventListener('keydown', (e) => {
@@ -132,17 +184,12 @@ window.addEventListener('load', function(){
       });
     }
     render(){
+      this.enemy.draw();
       this.player.update();
       this.player.draw();
-      // this.ball.update();
-      this.ball.draw();
+      this.ball.update();
       this.ball.iscollide();
-      // this condition calculate the Angle to redirect the Ball depend On where the ball collide with the Player
-      if (this.mouseX > this.player.posX && this.mouseX <= this.player.posX + this.player.width &&
-          this.mouseY >= this.player.posY && this.mouseY <= this.player.posY + this.player.height){
-              console.log(this.mouseY - this.player.posY - this.player.height/2, ((this.mouseY - this.player.posY - this.player.height/2) * 45) / (this.player.height / 2));
-      }
-      
+      this.ball.draw();
     }
     
   }
